d6cedd94e56198d98a3824b7df5156d06f8ffc1a
SmollSiD <sid-niro@yandex.ru>
2013-08-31 22:06:04 +0400

    remove of unnecessary code

--- server.py
+++ server.py
@@ -23,25 +23,4 @@ class Server():
         self.sock.close()
 serv=Server()
 serv.runServer()
-"""
-__author__ = 'user'
-import socket
-import os
-sock = socket.socket()
-sock.bind(('',6063))
-while True:
-    sock.listen(1)
-    conn,addr = sock.accept()
-    print(addr)
-    while True:
-        data=conn.recv(1024)
-        if not data:
-            break
-        str=data.decode('utf-8')
-    if str=="xbmc_on":
-        os.startfile("xbmc")
-    if str=="xbmc_off":
-        os.system("TASKKILL /IM xbmc.exe")
-    print(str)
-sock.close()
-"""
+
